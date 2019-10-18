6bbf5e2b6dfd1d5f3d0d7d1f40069bc99c6c86cf
luoqian67 <originalsvampire67@gmail.com>
2019-10-18 10:11:56 +0800

    修改启动脚本

---
+++ Centos7.6-kubernetes/node.sh
@@ -0,0 +1,23 @@
+# 允许 ssh连接
+sudo sed  -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sudo sed  -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
+# 关闭selinux
+sudo sed  -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+# 更换软件源 base epel pypi清华源
+sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+sudo curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo
+sudo mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
+sudo curl -o /etc/yum.repos.d/epel.repo http://mirrors.cloud.tencent.com/repo/epel-7.repo
+sudo yum makecache && yum update
+sudo pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+# 修改root密码
+echo 123456 | sudo passwd root --stdin
+# 添加默认网关和DNS
+sudo sed  -i "1aGATEWAY=192.168.137.1" /etc/sysconfig/network
+sudo sed  -i "1aDNS=8.8.8.8" /etc/sysconfig/network
+# 安装
+sudo yum install python net-tools -y
+# 清理缓存重启
+sudo yum clean all
+sudo reboot
+sudo route add default gw 192.168.137.1
